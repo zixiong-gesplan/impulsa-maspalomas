a475866061952fc4ac01f69fe67d29f8a4a01698
Zixiong Lin Yang <zlinyan@gesplan.es>
2025-05-12 08:43:09 +0100

    added workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://zixiong-gesplan.github.io',
+  base: 'impulsa-maspalomas',
   vite: {
     plugins: [tailwindcss()],
   },
