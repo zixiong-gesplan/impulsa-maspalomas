77636f095472577f4fb2a838ff684ba786ea29b1
Zixiong Lin Yang <zlinyan@gesplan.es>
2025-05-27 12:13:10 +0100

    Added icons

---
+++ src/consts/ASSETS.ts
@@ -0,0 +1,19 @@
+export const ASSETS = {
+    BACKGROUND: '/impulsa-maspalomas/background.png',
+    MINIMUN_FEE: '/impulsa-maspalomas/cota_minima.png',
+    MAXIMUM_FEE: '/impulsa-maspalomas/cota_maxima.png',
+    DIFFICULTY: '/impulsa-maspalomas/dificultad.png',
+    DUNIA: {
+        DEFAULT: '/impulsa-maspalomas/DUNIA_1.png',
+        POINTING: '/impulsa-maspalomas/DUNIA_2.png'
+    },
+    HEADER_MASDUNAS: '/impulsa-maspalomas/header-masdunas.png',
+    LONG_DISTANCE: '/impulsa-maspalomas/largo_recorrido.png',
+    LENGTH: '/impulsa-maspalomas/longitud.png',
+    MAP: '/impulsa-maspalomas/Mapa.png',
+    PATHS: {
+        UP: '/impulsa-maspalomas/Sendero_1.png',
+        MIDDLE: '/impulsa-maspalomas/Sendero_2.png',
+        DOWN: '/impulsa-maspalomas/Sendero_3.png'
+    }
+};
